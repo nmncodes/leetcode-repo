42158b5219c4d7aecca4b43fefcb1863050d098c
newman05 <namansurana4566@gmail.com>
2026-02-14 23:26:13 +0530

    biweekly176

---
+++ biweekly176/3838_Weighted_Word_Mapping.cpp
@@ -0,0 +1,27 @@
+/* 3838. Weighted Word Mapping
+ https://leetcode.com/contest/biweekly-contest-176/problems/weighted-word-m
+TC: O(n)
+SC: O(n)
+ Implementation problem
+*/
+
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        int n = words.size() ; 
+        string res = "" ; 
+        int sz = weights.size() ; 
+        for(int i = 0 ; i < n ; i++) {
+            int sum = 0 ;
+            for(int j = 0 ; j < (int)words[i].size() ; j++ ){
+                int num = words[i][j]  - 'a' ; 
+                sum += weights[num] ; 
+            }
+            sum = (sum % 26) ; 
+            char ch = 'a' + (25 - sum) ; 
+            res += ch ; 
+        }
+
+        return res ;
+    }
+};
